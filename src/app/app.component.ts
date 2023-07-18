51d24158407da6a63608f3bf52d24dc4b5f9f4a1
Fernando G. Luna <fer22luna@gmail.com>
2023-07-18 19:19:10 -0300

    Entrega Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
 
+type Product = {
+  name: String,
+  costs: String
+};
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +12,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'entregaAngular';
+  show : boolean = true;
+ 
+
+  products : Product[] = [
+    { name: 'Producto 1', costs: '100' },
+    { name: 'Producto 2', costs: '200' },
+    { name: 'Producto 3', costs: '300' },
+    { name: 'Producto 4', costs: '400' },
+    { name: 'Producto 5', costs: '500' },
+    { name: 'Producto 6', costs: '600' },
+    { name: 'Producto 7', costs: '700' },
+    { name: 'Producto 8', costs: '800' },
+    { name: 'Producto 9', costs: '900' },
+    { name: 'Producto 10', costs: '1000' }
+  ]
+
+  showTable(){
+    this.show = !this.show; 
+  }
+
+
 }
